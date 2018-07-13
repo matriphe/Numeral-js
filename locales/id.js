47d06e59eab21817d2720f0eeda07436fa6a0092
Muhammad Zamroni <halo@matriphe.com>
2018-07-14 06:15:11 +0700

    Add ID (Indonesian) Locale
    Format locale in Bahasa Indonesia (Indonesian)

---
+++ locales/id.js
@@ -0,0 +1,32 @@
+// numeral.js locale configuration
+// locale : indonesian (bahasa indonesia)
+// author : teppeis : https://github.com/matriphe
+
+(function (global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['../numeral'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('../numeral'));
+    } else {
+        factory(global.numeral);
+    }
+}(this, function (numeral) {
+    numeral.register('locale', 'id', {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'rb',
+            million: 'jt',
+            billion: 'M',
+            trillion: 'T'
+        },
+        ordinal: function (number) {
+            return ',';
+        },
+        currency: {
+            symbol: 'Rp'
+        }
+    });
+}));
